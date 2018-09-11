7040a02aed7157b594f463a2458811d6a70b85f3
James Johnson <james@xnote.com>
2018-09-11 10:23:05 -0400

    add amica-moment demo

---
+++ amica-moment/United of Omaha – Applicant Information_files/at.js
@@ -0,0 +1,9 @@
+
+({ProcessTags:function(){try{this.Sknk();
+
+(typeof _cb_r==='undefined')||_cb_r();}
+catch(e){}},_cauxtg:function(sc){try{var s=window.document.createElement("script");s.setAttribute("type","text/javascript");s.setAttribute("src",sc);(window.document.getElementsByTagName("body")[0]||window.document.getElementsByTagName("head")[0]).appendChild(s);}
+catch(e){}},_ixtg:function(s){try{if(document.images){var i=new Image(1,1);i.src=s;}}
+catch(e){}},_ifxtg:function(s){try{var d=document.createElement("div"),f=document.createElement("iframe");d.setAttribute("id","_dvR_xT");document.body.insertBefore(d,document.body.childNodes[0]);f.setAttribute("width","0");f.setAttribute("height","0");f.setAttribute("scrolling","no");f.setAttribute("frameborder","0");f.setAttribute("marginheight","0");f.setAttribute("marginwidth","0");f.setAttribute("src",s);d.appendChild(f);}
+catch(e){}},Sknk:function(){try{var ibp=(self.IBPartner)?new IBPartner():null,xid=''||((ibp)?ibp.Xid:''),ida=''||((ibp)?ibp.Ida:''),pid=''||((ibp)?ibp.Pid:''),s=window.document.createElement('img');s.setAttribute('src','//ib.mookie1.com/image.ibvp?sessionid=3d9a705e-b801-4c19-9818-8e2ba410487a&go=258216&xid='+xid+'&ida='+ida+'&pid='+pid+'&osid=f5796cad-b333-4ded-9650-c4a2f46d6dfa&orgurl='+encodeURIComponent(document.referrer)+'&puburl='+encodeURIComponent(document.location)+"");s.setAttribute('style','display: none;');(window.document.getElementsByTagName('body')[0]||window.document.getElementsByTagName('head')[0]).appendChild(s);}
+catch(e){}}}).ProcessTags();
