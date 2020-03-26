3bf4df443e3b14840de8e92c50dc0cacd85d8f09
David Stone <56355009+david-stone16@users.noreply.github.com>
2020-03-26 10:14:37 -0400

    - Screen labels no longer take the header value

--- NYL/assets/ngsd-header/load-headers.js
+++ NYL/assets/ngsd-header/load-headers.js
@@ -3,51 +3,13 @@ var blnHeaderIsScreenLabel = true;
 
 $(window).on('load', function() {
   handleHeaders();
-  displayHeaderAsScreenLabel();
-  $('.screen-label').text($('.screen-label').text().replace("HEADER_", ""));
 });
 
-$(document).on('click', '#progress-bar .steps .steps-text p', function () {
-  document.cookie = "screen=" + $('.navpanel a:contains(' + $(this).text() + ')');
-  displayHeaderAsScreenLabel();
-});
-$(document).on('click', '#dtc .navpanel .navpanel__item', function () {
-  document.cookie = "screen=" + $(this).text();
-  displayHeaderAsScreenLabel();
-});
-$(document).on('click', '#dtc .field-button', function () {
-  document.cookie = "screen=" + $('#dtc div.navpanel > div.navpanel__aligntop > div > a.navpanel__item.list-group-item.active + a').text();
-    displayHeaderAsScreenLabel();
-});
 /*$(window).resize(function() {
   handleHeaders();
   displayHeaderAsScreenLabel();
 });*/
 
-
-function displayHeaderAsScreenLabel() {
-  var count = 0;    
-  $(document).on('DOMSubtreeModified', '#dtc', function() {
-    if (count < 1) {
-      count++;
-      var screen = getCookie("screen");
-        var header = '';
-        if (isHeader($('#dtc div.navpanel > div.navpanel__aligntop > div > a.navpanel__item.list-group-item:contains(' + screen + ')'))) {
-            header = $('#dtc div.navpanel > div.navpanel__aligntop > div > a.navpanel__item.list-group-item:contains(' + screen + ')').text();
-        } else {
-            $('#dtc div.navpanel > div.navpanel__aligntop > div > a.navpanel__item.list-group-item:contains(' + screen + ')').prevAll().each(function () {
-                if (isHeader($(this))) {
-                    header = $(this).text();
-                    return false;
-                }
-            });
-        }        
-      if (blnHeaderIsScreenLabel) {
-        $('#dtc .screen-label').text(header)         
-      }
-    }
-  });
-}
 
 function handleHeaders() {
 $('.navpanel__item:contains(HEADER_)').each(function () {
--- NYL/ngsd-overrides.js
+++
@@ -1,9 +0,0 @@
-function getCookie(name) {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].split('=');
-        if (cookie[0].trim() == name) {
-            return decodeURIComponent(cookie[1]);
-        }
-    }
-}
